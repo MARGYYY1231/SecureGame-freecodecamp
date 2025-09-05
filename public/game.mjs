42e7d767f58eebacbc74640a59c18c21dfcbdda4
MARGYYY1231 <138099922+MARGYYY1231@users.noreply.github.com>
2025-09-05 15:02:14 +1200

    Added border to canvas

--- public/game.mjs
+++ public/game.mjs
@@ -4,3 +4,10 @@ import Collectible from './Collectible.mjs';
 const socket = io();
 const canvas = document.getElementById('game-window');
 const context = canvas.getContext('2d');
+
+console.log("Game script loaded");
+console.log(document.getElementById("game-window"));
+
+context.font = '40px sans-serif';
+context.fillStyle = "white";
+context.fillText("Hello Player!", canvas.width/2, canvas.height/2);
