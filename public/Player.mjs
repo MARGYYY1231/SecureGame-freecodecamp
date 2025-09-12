b51b0631fc6efaca4a020508c2aab7b2d653b09e
MARGYYY1231 <138099922+MARGYYY1231@users.noreply.github.com>
2025-09-12 14:13:04 +1200

    Try to animate slime

--- public/Player.mjs
+++ public/Player.mjs
@@ -4,97 +4,91 @@ class Player {
     this.y = y;
     this.score = score;
     this.id = id;
-    
-    this.frame = 0;
-    this.frameCounter = 0;
-    this.frameSpeed = 8;
-    this.isMoving = false;
-    this.direction = "D";
     this.speed = 2;
+    this.sheet = this.getSheet();
 
-    this.width = 64;
-    this.height = 64;
     this.frameX = 0;
     this.frameY = 0;
+    this.width = 64;
+    this.height = 64;
+    this.scale = 1;
 
-    this.movesheet = null;
-    this.idlesheet = null;
-
-    this.idleFrames = 6;
-    this.moveFrames = 8;
-
-    this.sheet = new Image();
-    this.sheet.src = "/assets/characters/Slime1_Idle_full.png";
-    this.sheetLoaded = false;
-    this.sheet.onload = () => {
-      this.sheetLoaded = true;
-    }
+    this.count = 0;
   }
 
-  setSheets(moveSheet, idleSheet){
-    this.movesheet = moveSheet;
-    this.idlesheet = idleSheet;
+  getSheet(){
+    const sheet = new Image();
+    sheet.src = "/assets/charcters/Slime1_Idle_full.png";
+    return sheet;
   }
 
   update(keys){
-    this.moving = false;
     if(keys["w"]){
       console.log("w");
       this.movePlayer("U", this.speed);
-      this.isMoving = true;
     }
     if(keys["s"]){
       console.log("s");
       this.movePlayer("D", this.speed);
-      this.isMoving = true;
     }
     if(keys["a"]){
       console.log("a");
       this.movePlayer("L", this.speed);
-      this.isMoving = true;
     }
     if(keys["d"]){
       console.log("d");
       this.movePlayer("R", this.speed);
-      this.isMoving = true;
     }
   }
 
 
   movePlayer(dir, speed) {
-    switch(dir.toUpperCase()){
-      case "U":
-        this.y -= speed;
-        this.frameY = 1;
-        break;
-      case "D":
-        this.y += speed;
-        this.frameY = 0;
-        break
-      case "L":
-        this.x -= speed;
-        this.frameY = 2;
-        break;
-      case "R":
-        this.x += speed;
-        this.frameY = 3;
-        break;
-    }
+    // switch(dir.toUpperCase()){
+    //   case "U":
+    //     this.y -= speed;
+    //     this.frameY = 1;
+    //     break;
+    //   case "D":
+    //     this.y += speed;
+    //     this.frameY = 0;
+    //     break
+    //   case "L":
+    //     this.x -= speed;
+    //     this.frameY = 2;
+    //     break;
+    //   case "R":
+    //     this.x += speed;
+    //     this.frameY = 3;
+    //     break;
+    // }
 
-    // animate regardless of state
-    this.frameTimer++;
-    if (this.frameTimer >= this.frameInterval) {
-      const maxFrames = this.moving ? this.moveFrames : this.idleFrames;
-      this.currentFrame = (this.currentFrame + 1) % maxFrames;
-      this.frameTimer = 0;
-    }
+    
   }
 
   draw(context){
-    //let sheet = this.isMoving ? this.movesheet : this.idlesheet;
     console.log("x: ", this.x);
     console.log("y: ", this.y);
-    context.drawImage(this.sheet, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
+    context.drawImage(
+      this.sheet, 
+      this.frameX * this.width, 
+      this.frameY * this.height, 
+      this.width, 
+      this.height, 
+      this.x, 
+      this.y, 
+      this.width * this.scale, 
+      this.height * this.scale
+    );
+
+    this.count++;
+    if(this.count > 5){
+      this.frameX++;
+      this.count = 0;
+    }
+
+    if(this.frameX > 5){
+      this.frameX = 0;
+    }
   }
 
   collision(item) {
--- public/game.mjs
+++ public/game.mjs
@@ -15,64 +15,22 @@ console.log(document.getElementById("game-window"));
 // window.addEventListener("keydown", e => keys[e.key.toLowerCase()] = true);
 // window.addEventListener("keyup", e => keys[e.key.toLowerCase()] = false);
 
-let player = new Player({x: 200, y: 200, score: 0, id:1});
-function gameloop(){
-    player.draw(context);
-}
-
-function frame(){
-    context.clearRect(0, 0 , gameWidth, gameHeight);
-    gameloop();
-    requestAnimationFrame(gameloop);
-}
-
-frame();
-
-// let player;
-
-// function loadImage(src) {
-//   return new Promise((resolve, reject) => {
-//     const img = new Image();
-//     img.src = src;
-//     img.onload = () => resolve(img);
-//     img.onerror = reject;
-//   });
-// }
-
-// function gameloop(){
-//     context.clearRect(0, 0, gameWidth, gameHeight);
-
 //     context.font = '20px sans-serif';
 //     context.fillStyle = "white";
 //     context.fillText("Controls: WASD", gameWidth/8, 30);
 //     context.fillText("Coin Race", gameWidth/4, 30);
 //     context.fillText("Rank: 1/1", gameWidth/4 * 3, 30);
 
-//     player.update(keys);
-//     player.draw(context);
+let player = new Player({x: 200, y:200, score: 0, id: 1});
 
-//   requestAnimationFrame(gameloop);
-// }
+function animate(){
+    player.draw(context);
+}
 
-// Promise.all([
-//   loadImage("assets/characters/Slime1_Idle_full.png"),
-//   loadImage("assets/characters/Slime1_Walk_body.png")
-// ]).then(([idleImage, moveImage]) => {
-//   player = new Player({ x: 200, y: 200, score: 0, id: 1,});
-//   player.setSheets(moveImage, idleImage);
-//   requestAnimationFrame(gameloop);
-// });
+function frame(){
+    context.clearRect(0, 0, width, height);
+    animate();
+    requestAnimationFrame(frame);
+}
 
-// let randX = Math.round(Math.random() * gameWidth);
-// let randY = Math.round(Math.random() * gameHeight);
-// const pancake = new Collectible({ x: randX, y: randY, value: 5, id: 3 });
-// pancake.draw(context);
-// randX = Math.round(Math.random() * gameWidth);
-// randY = Math.round(Math.random() * gameHeight);
-// const cake = new Collectible({ x: randX, y: randY, value: 2, id: 2 });
-// cake.draw(context);
-// randX = Math.round(Math.random() * gameWidth);
-// randY = Math.round(Math.random() * gameHeight);
-// const donut = new Collectible({ x: randX, y: randY, value: 1, id: 1 });
-// donut.draw(context);
-
+frame();
